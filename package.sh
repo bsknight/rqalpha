a5ca983523cfda0aa910094d75755935434aee59
root <bsknight58@gmail.com>
2017-08-01 16:16:36 +0800

    commit

--- package.sh
+++ package.sh
@@ -10,6 +10,11 @@ shc -r -f rqalpha/monitor.sh
 mv rqalpha/monitor.sh.x rqalpha/handler
 rm rqalpha/monitor.sh.x.c
 rm rqalpha/monitor.sh
+rm rqalpha/monitor.log
+rm rqalpha/dev.sh
+rm package.sh
+rm ./.git/ -rf
+rm ./.github/ -rf
 
 find ./  -path './zeus' -prune -name "*.py" | xargs python -m py_compile 
 find ./ -name "*.py.swp" | xargs rm -rf
@@ -20,14 +25,23 @@ find $WORKPATH/pkg/rqalpha/rqalpha/zeus -name "persist" | xargs rm -rf
 
 cd $WORKPATH/pkg/rqalpha/rqalpha/zeus/ffp
 tar -zcvf - ./ffp.py|openssl des3 -salt -k bsknight| dd of=ffp.xsc
+rm ./persist -rf
+
 cd $WORKPATH/pkg/rqalpha/rqalpha/zeus/ffr
 tar -zcvf - ./ffr.py|openssl des3 -salt -k bsknight| dd of=ffr.xsc
+rm ./persist -rf
+
 cd $WORKPATH/pkg/rqalpha/rqalpha/zeus/peg
 tar -zcvf - ./peg.py|openssl des3 -salt -k bsknight| dd of=peg.xsc
+rm ./persist -rf
+
 cd $WORKPATH/pkg/rqalpha/rqalpha/zeus/grcxcap
 tar -zcvf - ./grcxcap.py|openssl des3 -salt -k bsknight| dd of=grcxcap.xsc
+rm ./persist -rf
+
 cd $WORKPATH/pkg/rqalpha/rqalpha/zeus/grcxhs
 tar -zcvf - ./grcxhs.py|openssl des3 -salt -k bsknight| dd of=grcxhs.xsc
+rm ./persist -rf
 
 cd $WORKPATH/pkg/rqalpha
 find ./ -name "*.py" | xargs rm -rf
--- rqalpha/monitor.sh
+++ rqalpha/monitor.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 #set -x
-export JAVA_HOME=/usr/java/jre1.8.0_101
-export JRE_HOME=/usr/java/jre1.8.0_101
-export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib:$JRE_HOME/lib
-export ORACLE_HOME=/usr/lib/oracle/12.1/client64
-export TNS_ADMIN=$ORACLE_HOME/network/admin
-export PATH=$PATH:$ORACLE_HOME/bin::/usr/local/lib/:$JAVA_HOME/bin:$JRE_HOME/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib
+#export JAVA_HOME=/usr/java/jre1.8.0_101
+#export JRE_HOME=/usr/java/jre1.8.0_101
+#export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib:$JRE_HOME/lib
+#export ORACLE_HOME=/usr/lib/oracle/12.1/client64
+#export TNS_ADMIN=$ORACLE_HOME/network/admin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib
+#export PATH=$PATH:$ORACLE_HOME/bin:/usr/local/lib/:$JAVA_HOME/bin:$JRE_HOME/bin
+export PATH=$PATH:/usr/local/lib/:/usr/lib:/usr/lib64/
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/oracle/12.1/client64a/lib:/usr/lib:/usr/lib64/:/usr/local/lib
 export NLS_LANG="AMERICAN_AMERICA.ZHS16GBK"
 export NLS_DATE_FORMAT='YYYY-MM-DD'
 
 WORKSPACE_PATH='/home/workspace/rqalpha/rqalpha/zeus/'
-
+LOCAL_IP='172.16.50.141'
 names=('ffp' 'ffr' 'peg' 'grcxhs' 'grcxcap')
 #names=('ffp' 'ffr' 'peg')
 
@@ -26,17 +27,18 @@ for process_name in ${names[@]}; do
     else 
         today_date=`date +%Y%m%d%H%M%S`
         echo $today_date $process_name" exsit "$process_c
-        echo $today_date $process_name" exsit "$process_c | /usr/sbin/sendmail -t xiesc1@cjsc.com
+        (echo "Subject: $LOCAL_IP";echo $today_date $process_name" exsit "$process_c) | /usr/sbin/sendmail -t xiesc1@cjsc.com
         if [ "$process_c" -eq 0 ]; then
             echo 'restart...' 
             cd ${WORKSPACE_PATH}${process_name}/
             dd if=${process_name}.xsc |openssl des3 -d -k bsknight|tar zxf -
             /usr/bin/python /usr/bin/zeus run -fq 1m -rt p -f ${WORKSPACE_PATH}${process_name}/${process_name}.py -sc 1000000 -l verbose >> ${WORKSPACE_PATH}${process_name}/${process_name}.log 2>&1 &
             echo $! > ${WORKSPACE_PATH}${process_name}/pid
-            sleep 5
-            rm ${process_name}.py
         fi  
     fi
     echo "----------------------------------------"
 done
 echo "========================================"
+sleep 10
+cd $WORKSPACE_PATH
+find ./ -name "*.py" | xargs rm -rf
